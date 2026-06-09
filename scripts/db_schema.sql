a6339aa34bc19a4c678b71af2929c08e7e71df19
Tanveer Singh <152202845+tanveersingh005@users.noreply.github.com>
2026-06-09 15:53:06 +0530

    Created Database and Schema for different Layers

---
+++ scripts/db_schema.sql
@@ -0,0 +1,45 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseProject' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouseProject' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+
+USE master;
+GO
+
+-- Drop and Recreate the Database 'DataWarehouseProject' -- 
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouseProject')
+BEGIN
+	ALTER DATABASE DataWarehouseProject SET SINGLE_USER WITH ROLLBACK_IMMEDIATE;
+	DROP DATABASE DataWarehouseProject;
+	END
+GO;
+
+-- Create Database name 'DataWarehouseProject' --
+
+CREATE DATABASE DataWarehouseProject;
+
+USE DataWarehouseProject;
+GO
+
+-- Creating different schemas as per different layers --
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
